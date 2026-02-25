cbbc2901b1c9fa5ff1fc9c06b791aa4525b32fdf
Juan Camilo Alvarez <juancalvarez1116@gmail.com>
2026-02-25 01:06:13 -0500

    Class Date

---
+++ OPPConcepts/Frontend/Program.cs
@@ -0,0 +1,17 @@
+﻿using Backend;
+
+try
+{
+    var date1 = new Date();
+    var date2 = new Date(2026, 2, 29);
+    var date3 = new Date(2000, 2, 29);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
