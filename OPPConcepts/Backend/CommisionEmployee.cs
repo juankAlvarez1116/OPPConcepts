d6e313ba4bb93a6de89ec2707c94346963cba75e
Juan Camilo Alvarez <juancalvarez1116@gmail.com>
2026-03-05 17:08:58 -0500

    Add comission employee

---
+++ OPPConcepts/Backend/CommisionEmployee.cs
@@ -0,0 +1,56 @@
+﻿namespace Backend;
+
+public class CommisionEmployee : Employee
+{
+    // Fields
+
+    private float _commisionPercentaje;
+    private decimal _sales;
+
+    // Constructors
+
+    public CommisionEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate, float commisionPercentaje, decimal sales) :
+        base(id, firstName, lastName, isActive, bornDate, hireDate)
+    {
+        CommisionPercentaje = commisionPercentaje;
+        Sales = sales;
+    }
+
+    // Properties
+
+    public float CommisionPercentaje
+    {
+        get => _commisionPercentaje;
+        set => _commisionPercentaje = ValidateCommisionPercentaje(value);
+    }
+    public decimal Sales
+    {
+        get => _sales;
+        set => _sales = ValidateSales(value);
+    }
+
+    // Methods
+
+    public override decimal GetValueToPay() => (decimal)CommisionPercentaje * Sales;
+
+    public override string ToString() => base.ToString() + $"\n\t" +
+        $"Commision Percent: {CommisionPercentaje,20:P2}\n\t" +
+        $"Sales............: {Sales,20:C2}";
+
+    private float ValidateCommisionPercentaje (float commisionPercentaje)
+    {
+        if (commisionPercentaje < 0 || commisionPercentaje > 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(commisionPercentaje), "Sales must be between 0 and 1");
+        }
+        return commisionPercentaje;
+    }
+    private decimal ValidateSales(decimal sales)
+    {
+        if (sales < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(sales), "Sales must be greater than or equal to 0");
+        }
+        return sales;
+    }
+}
--- OPPConcepts/Frontend/Program.cs
+++ OPPConcepts/Frontend/Program.cs
@@ -12,8 +12,12 @@ try
     employees.Add(employee3);
     var employee4 = new HourlyEmployee(4040, "Kevin", "Rua", true, new Date(1998, 8, 20), new Date(2021, 6, 1), 10000, 112);
     employees.Add(employee4);
+    var employee5 = new CommisionEmployee(5050, "Sofia", "Gomez", true, new Date(1992, 12, 10), new Date(2018, 3, 15), 0.03f, 600000000);
+    employees.Add(employee5);
+    var employee6 = new CommisionEmployee(6060, "Diego", "Martinez", true, new Date(1985, 7, 30), new Date(2010, 9, 1), 0.03f, 120000000);
+    employees.Add(employee6);
 
-    foreach(var employee in employees)
+    foreach (var employee in employees)
     {
         Console.WriteLine(new string('-', 47));
         Console.WriteLine(employee);
